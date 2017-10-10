3540bde03ea41ad42c0117e10f2b9281bf89fc73
= <przemek.antoszek@gmail.com>
2017-10-10 23:14:38 +0200

    rownanie

---
+++ rownanie_kwadratowe.py
@@ -0,0 +1,9 @@
+a=input('podaj a: ')
+b=input('podaj b: ')
+c=input('podaj c: ')
+
+wczytane_dane = (a,b,c)
+print "Wczytane dane to: a=%d, b=%d, c=%d" %wczytane_dane
+
+delta = b**2 -(4*a*c)
+print "Delta wynosi: %.2f" %delta
