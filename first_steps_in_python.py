60d9c9ef1b8617d5a03eab82e60b8f09e348dc9e
= <przemek.antoszek@gmail.com>
2017-10-10 20:40:11 +0200

    pierwsze kroki w python

---
+++ first_steps_in_python.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+# imie = 'Przemek '
+# wiek = 24
+# srednia = 4.5
+# print imie + str(wiek) +" "+ str(srednia)
+#
+# tablica = []
+# tablica.append(1)
+# tablica.append("ala")
+# tablica.append(2)
+# tablica.append(4)
+# tablica.append(4.2)
+#
+# print "TABLICA"
+# for x in tablica:
+#     print x
+#
+# tablica2=[1,3,5,"ala ","nie ma ", 4.5]
+# print "TABLICA2"
+# for x in tablica2:
+#     print x
+#
+# tablica[0]=tablica2[2]
+# print "TABLICA"
+# for x in tablica:
+#     print x
+
+suma = 1 + 2 * 3 / 4.0
+print float(suma)
+print 11%3
+
+kwadrat = 7 ** 2
+szescian = 2 ** 3
+print kwadrat
+print szescian
+
+z1 = "witaj"
+z2 = "świecie"
