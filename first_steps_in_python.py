3573e2d89cac80c3b5cd189393004e22279a9ec6
= <przemek.antoszek@gmail.com>
2017-10-10 22:46:33 +0200

    new file - python_string.py

--- first_steps_in_python.py
+++ first_steps_in_python.py
@@ -1,29 +1,29 @@
 # -*- coding: utf-8 -*-
-# imie = 'Przemek '
-# wiek = 24
-# srednia = 4.5
-# print imie + str(wiek) +" "+ str(srednia)
-#
-# tablica = []
-# tablica.append(1)
-# tablica.append("ala")
-# tablica.append(2)
-# tablica.append(4)
-# tablica.append(4.2)
-#
-# print "TABLICA"
-# for x in tablica:
-#     print x
-#
-# tablica2=[1,3,5,"ala ","nie ma ", 4.5]
-# print "TABLICA2"
-# for x in tablica2:
-#     print x
-#
-# tablica[0]=tablica2[2]
-# print "TABLICA"
-# for x in tablica:
-#     print x
+imie = 'Przemek '
+wiek = 24
+srednia = 4.5
+print imie + str(wiek) +" "+ str(srednia)
+
+tablica = []
+tablica.append(1)
+tablica.append("ala")
+tablica.append(2)
+tablica.append(4)
+tablica.append(4.2)
+
+print "TABLICA"
+for x in tablica:
+    print x
+
+tablica2=[1,3,5,"ala ","nie ma ", 4.5]
+print "TABLICA2"
+for x in tablica2:
+    print x
+
+tablica[0]=tablica2[2]
+print "TABLICA"
+for x in tablica:
+    print x
 
 suma = 1 + 2 * 3 / 4.0
 print float(suma)
@@ -33,6 +33,14 @@ kwadrat = 7 ** 2
 szescian = 2 ** 3
 print kwadrat
 print szescian
- 
+
 z1 = "witaj"
 z2 = "świecie"
+
+tablica3 = tablica + tablica *3
+
+print "TABLICA 3 czyli polaczone tablice 1 i 2"
+for x in tablica3:
+    print x
+wiele_witaj = "witaj" *3
+print wiele_witaj
---
+++ python_string.py
@@ -0,0 +1,14 @@
+imie = "Przemek"
+wiek = 24
+print "witaj, %s" %imie
+print "witaj " +imie
+print "%s ma %d lata." %(imie,wiek)
+MojaTablica = [1,2,3]
+print "zawartosc mojej tablicy to: %s" %MojaTablica
+
+# krotka
+dane = ("Przemek", 5, 32)
+
+print "%s mieszka w bloku nr %d, mieszkanie nr %d" %dane
+dane2 = ("Darek","Jacek",44.4)
+print "Czesc %s i %s temperatura za oknem wnosi %.1f" %dane2
